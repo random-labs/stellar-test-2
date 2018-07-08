797019f80e95c179f46e4b928a2e67c5161f3b25
Zac Metin <zac.metin@gmail.com>
2018-07-08 21:23:26 +1200

    transaction builder wrapper

---
+++ src/TransactionFunctions.js
@@ -0,0 +1,25 @@
+
+
+StellarSdk.Network.useTestNetwork();
+// StellarBase.Network.usePublicNetwork(); if this transaction is for the public network
+
+
+function tBuilder(transaction){
+    let {address, sequenceNumber, payeeAddress, assetType, paymentAmount} = transactionDetails;
+    let account=new StellarBase.Account(transactionDetails);
+
+    let transaction = new StellarBase.TransactionBuilder(account)
+    // add a payment operation to the transaction
+    .addOperation(StellarBase.Operation.payment({
+      destination: payeeAddress,
+      asset: assetType,
+      amount: paymentAmount
+    }))
+    // .addOperation(StellarBase.Operation.setOptions({
+    //         signer: {
+    //             ed25519PublicKey: secondAccountAddress,
+    //             weight: 1
+    //         }
+    //     }))
+    .build();
+}
